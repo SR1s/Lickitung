fd3f8b6ad77f1f9cc496865d779b1c27c325c7fc
sr1s <seemsnevermind@gmail.com>
2017-11-30 16:47:56 +0800

    change app icon

---
+++ app/src/main/java/me/sr1/lickitung/MainActivity.java
@@ -0,0 +1,10 @@
+package me.sr1.lickitung;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SR1 on 2017/11/30.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
